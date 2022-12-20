f86f4002ca665b80799de3b4951d1634a5737515
Alex Lindemann <AlexDeadmanLindemann@gmail.com>
2022-12-20 08:30:30 -0600

    Testing

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
-const port = 3000
+const port = 3001
 
+app.use(cors())
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+const groups = ['Group One', 'Group Two', 'Group Three']
+
+app.get('/api/groups', (req, res) => {
+    let resGroups = [...groups];
+    console.log(req.query.name)
+    if (req.query.name) {
+        resGroups = groups.filter(group => group.indexOf(req.query.name) !== -1)
+    }
+    res.send(resGroups)
+})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
